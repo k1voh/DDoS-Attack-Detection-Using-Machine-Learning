0d2acd7278280548b6bdc0a2fd0a9fcc5a17eecc
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-10-09 02:28:16 +0530

    Working Code

--- Model/Detect.py
+++ Model/Detect.py
@@ -32,20 +32,21 @@ import json
 ''' REMOVE '#' AFTER CREATING AND JOINING FRONT-END
     AND REMOVE X=inputSet'''
 
-def detectDdos(json_input):
-    inputSet = pd.DataFrame(pd.json_normalize(json_input))
+def detectDdos(inputSet):
     inputSet['new_SRC_IP'] = LabelEncoder().fit_transform(inputSet['Src IP'])
     inputSet['new_DST_IP'] = LabelEncoder().fit_transform(inputSet['Dst IP'])
     inputSet['new_Timestamp'] = LabelEncoder().fit_transform(inputSet['Timestamp'])
     
     X = inputSet.drop(['Timestamp','Src IP','Dst IP'], axis='columns')
-    X = inputSet
     with open("Model/Detect_DDoS.pickle", "rb") as pickle_model:
         model = pickle.load(pickle_model)
         
-    return model.predict(inputSet)
+    return model.predict(X)
 
 ''' REMOVE BELOW CODE AFTER FRONT-END '''
-#json_input = json.loads('{"Fwd Seg Size Min":20,"Flow IAT Min":13,"Src Port":50018,"Tot Fwd Pkts":3,"Init Bwd Win Byts":149,"new_SRC_IP":4367,"new_DST_IP":3370,"new_Timestamp":46713}')
 
-#print(detectDdos(json_input))
+# json_input = json.loads('{"Fwd Seg Size Min":20,"Flow IAT Min":13,"Src Port":50018,"Tot Fwd Pkts":3,"Init Bwd Win Byts":149,"new_SRC_IP":4367,"new_DST_IP":3370,"new_Timestamp":46713}')
+
+# json_input = json.loads('{"Fwd Seg Size Min":0,"Flow IAT Min":4,"Src Port":80,"Tot Fwd Pkts":4,"Init Bwd Win Byts":211,"new_SRC_IP":3464,"new_DST_IP":3656,"new_Timestamp":10482}')
+
+# print(detectDdos(json_input))
